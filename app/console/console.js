09dc4a2e6b1d25df1c3cf16c647242f0226a1684
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-06-08 22:13:55 +0900

    add-console

---
+++ app/console/console.js
@@ -0,0 +1,8 @@
+console.log('hello world')
+
+console.log('hello %s', 'world')
+
+console.error(new Error('Whoops, something bad happened'))
+
+const name = 'Will Robinson'
+console.warn(`Danger ${name}! Danger!`)
